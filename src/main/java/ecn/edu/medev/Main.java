7bfc4f500c70d3179ba2545b1ff8ba143edd3c55
ibrahim-9999 <79332013+Alexis-Ibrahim@users.noreply.github.com>
2023-01-25 10:39:40 +0100

    Update packages and main.java

---
+++ src/main/java/ecn/edu/medev/Main.java
@@ -0,0 +1,14 @@
+package ecn.edu.medev;
+
+/**
+ * Classe principale du projet contenant la methode main
+ */
+public class Main {
+    /**
+     * Méthode main permettant le lancement 
+     * @param args liste des paramètres à fournir en ligne de commande 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
